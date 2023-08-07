e70c777a13ab2b518de75a104b329fccabfa9048
jordanlianhs <97859704+jordanlianhs@users.noreply.github.com>
2023-08-07 17:43:49 +0800

    Deploying to github pages

--- cgpafrontend/src/App.js
+++ cgpafrontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/CGPACalculator' element={<Home />} />
           <Route path='/converter' element={<Converter />} />
           <Route path='/addcourse' element={<AddCourse />} />
           <Route path='/addcourses' element={<AddCourses />} />
